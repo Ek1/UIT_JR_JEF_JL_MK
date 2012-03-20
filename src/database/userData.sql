b8afefe5686e524733751ad2cd402843e728ac49
Jérémie Lagraviere <mrjay@mrjay-PC.(none)>
2012-03-20 09:52:18 +0200

    Some user : 5 admins, 2 'normal' users

---
+++ src/database/userData.sql
@@ -0,0 +1,7 @@
+INSERT INTO "User" VALUES(1,'mrjay','mrjay','Jeremie','Lagraviere',1);
+INSERT INTO "User" VALUES(2,'erkki','erkki','Erkki','Seppä',1);
+INSERT INTO "User" VALUES(4,'wheany','wheany','Jan-Erik','Finnberg',1);
+INSERT INTO "User" VALUES(5,'marcus','marcus','Marcus','Kossatz',1);
+INSERT INTO "User" VALUES(6,'jukka','jukka','Jukka','Ruohonen',1);
+INSERT INTO "User" VALUES(7,'myUser1','myUser1','John','Doe',0);
+INSERT INTO "User" VALUES(8,'myUser2','myUser2','Docteur','Who',0);
