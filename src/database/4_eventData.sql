7b52c241287f9ad4df6126aca5bc8fb3fe36adfe
mrjay42 <mrjay42@gmail.com>
2012-03-20 21:34:38 +0200

    Adding some data for the event table I will add more soon, if needed...

---
+++ src/database/4_eventData.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Event" VALUES(1,'starting the kernel','not a big stuff as GNU, just for fun ^^','1991-10-05 00:00:00.000',15,1);
+INSERT INTO "Event" VALUES(2,'meeting about the bugs','big meeting everyone should come','2012-03-12 00:00:00.000',20,2);
+INSERT INTO "Event" VALUES(3,'debugging the network stack','big work concerning the network configuration','1995-02-01 00:00:00.000',35,1);
+INSERT INTO "Event" VALUES(4,'Reading the stuff','creating the reading part of the system','1970-12-05 00:00:00.000',5,3);
