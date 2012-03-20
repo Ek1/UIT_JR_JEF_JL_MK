59b56b37490f389a94c7dae0803fedc0c68cd94c
mrjay42 <mrjay42@gmail.com>
2012-03-20 21:15:54 +0200

    Adding some data for the table project

---
+++ src/database/3_projectData.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Project" VALUES(1,'GNU/Linux',1,2,3);
+INSERT INTO "Project" VALUES(2,'Language C',1,3,6);
+INSERT INTO "Project" VALUES(3,'Turing machine ',1,1,2);
+INSERT INTO "Project" VALUES(4,'First compiler ever',1,4,5);
+INSERT INTO "Project" VALUES(5,'Sponge bob in space',1,5,1);
