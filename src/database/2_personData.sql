bd1fa4413fe58ab1acc4c2f02ee82971fcffd5df
mrjay42 <mrjay42@gmail.com>
2012-03-20 21:09:43 +0200

    Rename the sql files, to show explicitly the order of insertions... one sql data file per table : userData => table user, personData => table person, etc.

---
+++ src/database/2_personData.sql
@@ -0,0 +1,7 @@
+INSERT INTO "Person" VALUES(1,'Mikael','Hawking');
+INSERT INTO "Person" VALUES(2,'Alan','Turing');
+INSERT INTO "Person" VALUES(3,'Linux','Torvalds');
+INSERT INTO "Person" VALUES(4,'Richard','Stallman');
+INSERT INTO "Person" VALUES(5,'Grace','Hopper');
+INSERT INTO "Person" VALUES(6,'Dennis','Ritchie');
+INSERT INTO "Person" VALUES(7,'Brian','Kernighan');
